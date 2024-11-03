fdda9ad5960968d5d3ccd37c7d16fa291e38a9dd
Muhammad Assad <muhammad@assad.ca>
2024-11-02 21:22:06 -0700

    beginning setup for new spritesheet

--- src/systems/world_init.hpp
+++ src/systems/world_init.hpp
@@ -14,6 +14,8 @@ Entity createEmptyButton(const Entity& entity, vec2 position, vec2 scale, const 
 Entity createChangeSceneButton(const Entity& entity, vec2 position, vec2 scale, const std::string& label, const std::string& textureName, const std::string& nextScene);
 Entity createResumeButton(const Entity& entity, vec2 position, vec2 scale, const std::string& label, const std::string& textureName);
 Entity createChangeSceneButton(const Entity& entity, vec2 position, vec2 scale, const std::string& label, const std::string& nextScene);
-Entity createSpriteSheet(const Entity& entity, vec2 position, float sheetWidth, float sheetHeight, float cellWidth, float cellHeight, const std::vector<unsigned int>& animationFrames);
+Entity createSpriteSheet(const Entity& entity, vec2 position, float sheetWidth, float sheetHeight, float cellWidth,
+                         float cellHeight, const std::vector<unsigned int>& animationFrames,
+                         const std::string textureName = "turtle_sprite_sheet");
 
 void setZone(Entity entity, ZONE_TYPE zType, vec2 position);
