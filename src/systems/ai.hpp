cb9beeb1fe3cc4552d3d248f79c343f6baf20192
Jackson Li <jacksonli2001@hotmail.com>
2024-11-03 12:43:59 -0800

    Make turtle sleep appropriately

--- src/systems/ai.hpp
+++ src/systems/ai.hpp
@@ -8,5 +8,8 @@
 class AISystem {
   public:
     void step(float elapsed_ms);
-    void updateDash();
+    void updateDash(float elapsed_ms);
+private:
+    float timeAccumulator = 0.0f;
+    static float calculateDistanceSquared(const vec2& position1, const vec2& position2);
 };
