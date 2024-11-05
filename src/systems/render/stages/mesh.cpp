3fcdaa7d041118fd9eb435501ec968ada20b1870
Muhammad Assad <muhammad@assad.ca>
2024-11-04 19:11:33 -0800

    updated values for level 6

--- src/systems/render/stages/mesh.cpp
+++ src/systems/render/stages/mesh.cpp
@@ -124,11 +124,8 @@ void MeshStage::drawMesh(const Entity& entity) {
     // Checking to see if we should light up this mesh 
     GLint light_up_uloc = glGetUniformLocation(shader, "light_up");
     assert(light_up_uloc >= 0);
-    if (registry.litEntities.has(entity)) {
-        glUniform1i(light_up_uloc, 1);
-    } else {
-        glUniform1i(light_up_uloc, 0);
-    }
+
+    glUniform1i(light_up_uloc, registry.litEntities.has(entity));
 
     // Drawing of num_indices/3 triangles specified in the index buffer
     GLint size = 0;
